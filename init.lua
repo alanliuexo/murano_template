8421db6f8db8f8ecce0a542bee577fd75454b582
Alan Liu <alanliu@exosite.com>
2019-09-24 19:09:45 +0800

    add files

---
+++ init.lua
@@ -0,0 +1,13 @@
+---
+-- Bring this solution up to where we expect
+
+-- require('migrations/1_Welcome')
+-- require('migrations/2_SetupAssetsSchema')
+-- require('migrations/3_SetupNotifierSchema')
+-- require('migrations/4_StartupTimeouts')
+-- require('migrations/5_StatusifyOldRules')
+
+-- Remember to add new migrations here.
+
+-- local MGR = require('migrator')
+-- MGR.up()
