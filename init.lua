dbd83e6cf06572533516bfb99eb540a96e26f826
Alan Liu <alanliu@exosite.com>
2019-09-25 14:27:35 +0800

    use init.lua

--- init.lua
+++ init.lua
@@ -11,3 +11,12 @@
 
 -- local MGR = require('migrator')
 -- MGR.up()
+
+local param = {
+    service='webservice',
+    parameters={
+        headers={}
+    }
+}
+param.parameters.headers["content-security-policy"] = "frame-ancestors 'self'; img-src data: blob: *; media-src data: blob: *; default-src 'unsafe-eval' 'unsafe-inline' 'self' data: blob: ws: wss: *.exosite.io *.auth0.com sentry.io fonts.googleapis.com cdn.jsdelivr.net cdn.mxpnl.com api.mixpanel.com fonts.gstatic.com *.amazonaws.com *.youtube.com youtu.be *.ytimg.com *.test2.com"
+Config.setParameters(param)
