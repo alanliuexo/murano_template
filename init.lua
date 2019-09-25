64fcb3839dd5975fc2fec002a9f5691f46e9fe06
Alan Liu <alanliu@exosite.com>
2019-09-25 16:41:59 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -14,12 +14,14 @@
 
 local param = Config.getParameters({service='webservice2'})
 log.info(to_json(param))
--- local param = {
---     service='webservice',
---     parameters={
---         headers={}
---     }
--- }
+if param.error then
+    param = {
+        service='webservice',
+        parameters={
+            headers={}
+        }
+    }
+end
 param.service = 'webservice'
 param.parameters.headers["content-security-policy"] = "frame-ancestors 'self'; img-src data: blob: *; media-src data: blob: *; default-src 'unsafe-eval' 'unsafe-inline' 'self' data: blob: ws: wss: *.exosite.io *.auth0.com sentry.io fonts.googleapis.com cdn.jsdelivr.net cdn.mxpnl.com api.mixpanel.com fonts.gstatic.com *.amazonaws.com *.youtube.com youtu.be *.ytimg.com *.test4.com"
 Config.setParameters(param)
